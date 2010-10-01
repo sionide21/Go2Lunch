45895672a60045740d5616586153d5a943991672
Matt Moriarity <matt.moriarity@gmail.com>
2010-09-30 22:29:52 -0400

    Added working update checking. No automatic installs yet.

--- update.go
+++ update.go
@@ -5,23 +5,66 @@ import (
 	"io/ioutil"
 	"http"
 	"fmt"
+	"strings"
 )
 
-func CheckForUpdates() (err os.Error) {
-	res, _, err := http.Get("http://github.com/mjm/Go2Lunch/raw/master/README.md")
-	if res.StatusCode != 200 {
-		err = os.NewError(fmt.Sprint("Response had status code ", res.StatusCode))
-		return
+/*
+	Various URLs used for automatically updating.
+*/
+const (
+	currentVersionUrl = "http://github.com/hgp/Go2Lunch/raw/master/VERSION"
+	downloadUrl = "http://github.com/downloads/hgp/Go2Lunch/lunch"
+)
+
+func getCurrentVersion() (string, os.Error) {
+	res, _, err := http.Get(currentVersionUrl)
+	if err != nil {
+		return "", err
 	}
-	buf, _ := ioutil.ReadAll(res.Body)
+	
+	if res.StatusCode != 200 {
+		return "", os.NewError(fmt.Sprint("Could not check current version. Status code from server: ", res.StatusCode))
+	}
+	
+	buf, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		return "", err
+	}
+	
 	res.Body.Close()
-	fmt.Printf("%s\n", buf)
-	return
+	return fmt.Sprintf("%s", buf), nil
 }
 
-func main() {
-	err := CheckForUpdates()
+func needsUpdate() (bool, os.Error) {
+	currentVersion, err := getCurrentVersion()
 	if err != nil {
-		panic(err)
+		return false, err
 	}
+	
+	return currentVersion != clientVersion, nil
+}
+
+func CheckForUpdates() (err os.Error) {
+	update, err := needsUpdate()
+	if err != nil {
+		return
+	}
+	
+	if update {
+		fmt.Print("An update is available. Would you like to download it? [Y/n] ")
+		var result string
+		fmt.Scanln(&result)
+		
+		fmt.Println()
+		
+		switch strings.ToLower(result) {
+		case "", "y":
+			fmt.Println("Automatic downloading of updates is not available yet.")
+			fmt.Println("You can download the new version at", downloadUrl)
+			fmt.Println()
+			return
+		}
+		
+	}
+	return
 }
