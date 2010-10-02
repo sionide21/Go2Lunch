7fd90b4d4f2d599b0a3f204419d2baac412b7fc1
Matt Moriarity <matt.moriarity@gmail.com>
2010-10-02 13:59:56 -0400

    Adding some debugging for the update script

--- update_client.sh
+++ update_client.sh
@@ -6,5 +6,7 @@ replacement_path=$2
 echo Copying updated version...
 sleep 3
 
+echo cp $replacement_path $lunch_path
 cp $replacement_path $lunch_path
+chmod u+x $replacement_path
 echo Updated version installed.
