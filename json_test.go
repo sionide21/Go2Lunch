ac2c557dae1d238bedb478b0d709c53a830534ff
Ben Olive <ben.olive@gatech.edu>
2010-11-06 17:54:28 -0400

    Runnable Test

--- json_test.go
+++ json_test.go
@@ -3,18 +3,27 @@ package main
 import (
 	"testing"
 	"json"
+	"regexp"
 )
 
+var tests = []testing.Test{
+	{"main.TestMarshall", TestMarshall},
+}
+
 func TestMarshall(t *testing.T) {
-	bin := make(Bin, 10)
+	bin := interface{}(make(Bin, 10))
 
 	_, ok := bin.(json.Marshaler)
 	if !ok {
 		t.Fatal("main.Bin not a json.Marshaler")
 	}
 
-	_, ok := bin.(json.Unmarshaler)
+	_, ok = bin.(json.Unmarshaler)
 	if !ok {
 		t.Fatal("main.Bin not a json.Unmarshaler")
 	}
+}
+
+func main() {
+	testing.Main(regexp.MatchString, tests)
 }
