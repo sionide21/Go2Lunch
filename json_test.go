9a815ea08e29fc70ac84d34b3dc86fcb50aa1709
andy <andy@bigandian.com>
2010-11-06 17:55:36 -0400

    Adde pointer tests.

--- json_test.go
+++ json_test.go
@@ -8,6 +8,17 @@ import (
 func TestMarshall(t *testing.T) {
 	var bin interface{}
 	bin = make(Bin, 10)
+	pbin := &bin
+
+	_, ok := pbin.(json.Marshaler)
+	if !ok {
+		t.Fatal("*main.Bin not a json.Marshaler")
+	}
+
+	_, ok := pbin.(json.Unmarshaler)
+	if !ok {
+		t.Fatal("*main.Bin not a json.Unmarshaler")
+	}
 
 	_, ok := bin.(json.Marshaler)
 	if !ok {
